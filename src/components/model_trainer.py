4cd2d746889c861a351c05f331928d83071a76e4
Varun <sinhavarun257@gmail.com>
2024-08-09 01:31:01 +0530

    Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,75 @@
+import os
+import sys
+from dataclasses import dataclass
+
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import save_obj
+from src.utils import evaluate_model
+
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path: str = os.path.join("artifacts", 'model.pkl')
+    
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config = ModelTrainerConfig()
+        
+    def initiate_model_training(self, train_array, test_array):
+        try:
+            logging.info("Split train and test data into features and target")
+            X_train, y_train , X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+            models = {
+                "Linear Regression": LinearRegression(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest": RandomForestRegressor(),
+                "AdaBoost": AdaBoostRegressor(),
+                "Gradient Boosting": GradientBoostingRegressor(),
+                "XGBoost": XGBRegressor(),
+                "KNN": KNeighborsRegressor(),
+                "CatBoost": CatBoostRegressor(verbose=False)
+            }
+        
+            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            
+            best_model_score = max(sorted(model_report.values()))
+            
+            best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            
+            best_model = models[best_model_name]
+            
+            if best_model_score < 0.5:
+                logging.error("Model score is less than 0.5")
+                raise CustomException("No best model found")
+            logging.info("Best model found on both train and test data")
+            
+            save_obj(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            
+            predicted = best_model.predict(X_test)
+            r2 = r2_score(y_test, predicted)
+            
+            return r2
+        except Exception as e:
+            logging.error("Model Training failed")
+            raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from src.exception import CustomException
 
+from sklearn.metrics import r2_score
+
 def save_obj(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,5 +16,26 @@ def save_obj(file_path,obj):
         with open(file_path, 'wb') as file__obj:
             pickle.dump(obj, file__obj)
             
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        
+        for model_name in models:  # Loop directly over the keys in the models dictionary
+            model = models[model_name]  # Access each model by its name
+            model.fit(X_train, y_train)
+            
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            
+            report[model_name] = test_model_score  # Store the test score with the model name as the key
+        
+        return report
+    
     except Exception as e:
         raise CustomException(e, sys)

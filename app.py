d175b0a8fb27e485149bc30a751708e94ecad24f
Varun <sinhavarun257@gmail.com>
2024-08-25 12:39:15 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -39,6 +39,5 @@ def predict_datapoint():
     
     
 
-if __name__=="__main__":
-    # app.run(host="0.0.0.0",port=8080)        
+if __name__=="__main__":      
     app.run(host='0.0.0.0', port=5000, debug=True)        
